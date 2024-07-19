b0c0f0c2e2f1cb62ab3f023d7f962c2a01076cb6
kimjaehyuk <jhkim0097@gmail.com>
2024-07-19 20:18:53 +0900

    [fix] remove redirect_uri GGMATCH-49

--- app/services/forty_two_api_service.py
+++ app/services/forty_two_api_service.py
@@ -5,9 +5,8 @@ from app.exceptions.http_exceptions import NotFoundException, UnauthorizedExcept
 
 class FortyTwoAPIService:
     def __init__(self):
-        self.base_url = settings.forty_two_api_base_url
-        self.client_id = settings.oauth_client_id
-        self.client_secret = settings.oauth_client_secret
-        self.redirect_uri = settings.oauth_redirect_uri
+        self.base_url = settings.FORTY_TWO_API_BASE_URL
+        self.client_id = settings.FORTY_TWO_OAUTH_CLIENT_ID
+        self.client_secret = settings.FORTY_TWO_OAUTH_CLIENT_SECRET
 
 forty_two_api_service = FortyTwoAPIService()
