fee0c03d3918121018b372b3b45a8f9779afe974
kimjaehyuk <jhkim0097@gmail.com>
2024-07-19 20:18:31 +0900

    [Feat] add SessionMiddleware GGMATCH-49

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, APIRouter
 from app.api.v1.endpoints import auth, evaluation, user
+from app.core.config import settings
+from starlette.middleware.sessions import SessionMiddleware
 from app.exceptions.handlers import (
     not_found_exception_handler,
     unauthorized_exception_handler,
@@ -10,21 +12,25 @@ from app.exceptions.handlers import (
 from app.exceptions.http_exceptions import NotFoundException, UnauthorizedException, ForbiddenException, BadRequestException
 
 # FastAPI 인스턴스, docs 설정
-app = FastAPI(    title="42gg.match.server API",
+app = FastAPI(
+    title="42gg.match.server API",
     description="API documentation for 42gg.match.server",
     version="0.1.0",
     docs_url="/api/docs",  # Swagger UI 경로 설정
     redoc_url="/api/redoc"  # ReDoc 경로 설정
 )
 
+# SessionMiddleware 추가
+app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY, same_site="Lax")
+
 # APIRouter 설정
-api_v1_router = APIRouter(prefix="/api/v1")
+api_v1_router = APIRouter(prefix=settings.API_V1_URI)
 api_v1_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_v1_router.include_router(evaluation.router, prefix="/evaluations", tags=["evaluations"])
 api_v1_router.include_router(user.router, prefix="/users", tags=["users"])
 app.include_router(api_v1_router)
 
-# Gloval Exception Handlers
+# Global Exception Handlers
 app.add_exception_handler(NotFoundException, not_found_exception_handler)
 app.add_exception_handler(UnauthorizedException, unauthorized_exception_handler)
 app.add_exception_handler(ForbiddenException, forbidden_exception_handler)
