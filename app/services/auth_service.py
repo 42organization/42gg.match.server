cb4320d7385228fae75ad7d650cba71c61d93f7e
kimjaehyuk <jhkim0097@gmail.com>
2024-07-23 22:13:42 +0900

    [Fix] KeyError ValueError처리, profile_image get()으로 변경 GGMATCH-19

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,7 +1,6 @@
 from app.schemas.auth import FortyTwoToken
 from app.schemas.user import User
 from app.services.forty_two_api_service import forty_two_api_service
-from app.utils.jwt_token_provider import jwt_provider
 
 class AuthService():
     def __init__(self):
@@ -12,17 +11,19 @@ class AuthService():
         :param token: 42 토큰
         :return: JWT 토큰
         """
-        forty_two_user_info = await forty_two_api_service.get_user_info_by_forty_two_access_token(token['access_token'])
-        user = User(
-            forty_two_id = forty_two_user_info["id"],
-            intra_id = forty_two_user_info["login"],
-            profile_image = forty_two_user_info["image"]["link"],
-            email = forty_two_user_info["email"],
-            forty_two_access_token = token
-        )
-        return jwt_provider.create_access_token(data=user.dict())
-
-
+        try:
+            forty_two_user_info = await forty_two_api_service.get_user_info_by_forty_two_access_token(
+                token["access_token"])
+            user = User(
+                forty_two_id=forty_two_user_info["id"],
+                intra_id=forty_two_user_info["login"],
+                profile_image=forty_two_user_info.get("image", {}).get("link"),
+                email=forty_two_user_info["email"],
+                forty_two_access_token=token
+            )
+            return user
+        except KeyError as e:
+            raise ValueError(f"Missing essential user information: {e}")
 
 
 # class User(BaseModel):
