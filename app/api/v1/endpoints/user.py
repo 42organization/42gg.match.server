df3b7e9d116f4da6172041b909a52e9ec1a9c9a5
kimjaehyuk <jhkim0097@gmail.com>
2024-07-29 16:05:01 +0900

    [Fix] [GGMATCH-51] 내정보조회 "/me" 엔드포인트 추가

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -1,7 +1,21 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from app.api.v1.deps import get_current_user, CurrentUser
+from app.schemas.user import User
+
 router = APIRouter()
 
 
 @router.get("")
 def users():
     return "This is User!"
+
+
+@router.get("/me", response_model=User, status_code=status.HTTP_200_OK)
+def me(current_user: CurrentUser):
+    """
+    헤더의 Bearer 토큰을 토대로 사용자 정보를 반환합니다.
+    :param current_user: \[User\] 사용자 정보
+    :return: \[User\] 사용자 정보
+    """
+    return current_user
